cce685ebe1baf9d221bc259bcb056b7ad8994df8
j10 <jasontenczar@gmail.com>
2026-04-22 16:43:23 -0400

    testing ready

--- test_local/localtest1/run_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-DIR="$(cd "$(dirname "$0")" && pwd)"
-
-osascript -e "tell app \"Terminal\" to do script \"cd '$DIR' && python3 ../../peerProcess.py 1001\""
-sleep 1
-osascript -e "tell app \"Terminal\" to do script \"cd '$DIR' && python3 ../../peerProcess.py 1002\""
-sleep 1
-osascript -e "tell app \"Terminal\" to do script \"cd '$DIR' && python3 ../../peerProcess.py 1003\""
